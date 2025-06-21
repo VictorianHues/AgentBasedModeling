c4e57f3a198293663866a7f02f168b6a205f84c7
Henry Zwart <henry.zwart@dragonfly.co.nz>
2025-06-21 12:18:52 +0200

    add vectorised model to documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,12 @@ release = "0.0.1"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "sphinx.ext.viewcode"]
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.mathjax",
+]
 
 templates_path = ["_templates"]
 exclude_patterns = []
